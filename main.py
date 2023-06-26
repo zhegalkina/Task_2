6462e722767ae53f6aa02cc47fbc44b1edca25fb
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-26 21:11:42 +0300

    task_2

---
+++ main.py
@@ -0,0 +1,34 @@
+"""№ 1"""
+
+# text = input("Enter some words: ")
+# text_list = [text.lower() for text in text.split()]
+# text_list.sort()
+# print(text_list)
+
+
+"""№ 2"""
+# import itertools
+# for x in list(itertools.combinations('123', 3)):
+#     print(x)
+
+
+"""№ 3"""
+# import itertools
+# digits = [1, 2, 3]
+# for x in itertools.permutations(digits):
+#     print(list(x))
+
+
+"""№ 4"""
+# import random
+# list_1 = [5, 7, 1000, 999, 6363]
+# print(random.sample(list_1, 3))
+
+"""№ 5"""
+
+# def func_sum(*args):
+#     sum=0
+#     for n in args:
+#         sum+=n
+#     print(sum)
+# func_sum(1,33,4,6,7,8,99)
